b2eab5b908e9321331d32122225303a3dcb2b406
Daniel Magalhães Bicalho <danielmbicalho@users.noreply.github.com>
2022-09-27 17:16:30 -0300

    Correção
    correção dos caminhos e do comando unzip

--- apache_iac.sh
+++ apache_iac.sh
@@ -3,5 +3,6 @@
 apt update 
 apt upgrade -y
 apt install -y apache2 unzip
-curl -LO https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip -O /tmp
+curl -LO https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip -O --output-dir /tmp
+unzip /tmp/main.zip -d /tmp
 cp -r /tmp/linux-site-dio-main/ /var/www/html/
